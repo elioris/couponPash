dc36700df81e3354c64a2de1481fa8b1a01a5402
איש שלום < @home>
2017-04-17 21:43:06 +0300

    filter

---
+++ src/com/elior/coupons/utilss/Filter1.java
@@ -0,0 +1,36 @@
+package com.elior.coupons.utilss;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter("/rest/api/*")
+public class Filter1 implements Filter {
+
+ 
+  
+	public void destroy() {
+		
+	}
+
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	
+		
+		chain.doFilter(request, response);
+	}
+
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+	
+	}
+
+}
